a65a500994d8c0b9935f5655ea9a02cde10879b5
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-10-19 11:58:41 +0530

    team leader commited

---
+++ src/SimpleInterestCalculator.java
@@ -0,0 +1,11 @@
+
+public class SimpleInterestCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        public static double calculateSimpleInterest(double p,double r,double t) {
+        	//complete the code
+        }
+	}
+
+}
